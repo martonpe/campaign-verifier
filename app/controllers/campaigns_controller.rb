fc0cd65c816cd242417f10a5da1ef8b2702ffe14
Peter Marton <martonpe@secretsaucepartners.com>
2018-01-28 19:38:49 +0100

    Implement verify in CampaignsController

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,22 +1,48 @@
+require 'net/http'
+
 class CampaignsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def verify
     payload = {
-      campaigns: [
-        {
-          id: 1,
-          status: {
-            value: 0,
-            consistent: true
-          },
-          ad_description: {
-            value: "Description for campaign 11",
-            consistent: false
-          }
+      campaigns: []
+    }
+
+    statuses = {
+      'active' => 'enabled',
+      'paused' =>'disabled',
+      'deleted' => 'disabled'
+    }
+
+    ad_service_url = ENV['AD_SERVICE_URL']
+    res = Net::HTTP.get(URI.parse(ad_service_url))
+    ads = JSON.parse(res)['ads']
+
+    params['campaigns'].each do |c|
+      campaign = Campaign.find(c['id'])
+
+      ad = ads.detect {|elem| elem['reference'] == campaign.external_reference.to_s }
+
+      if not ad then
+        payload[:campaigns] << { id: campaign.id, missing: true }
+        next
+      end
+
+      campaign_payload = {
+        id: campaign.id,
+        status: {
+          local_value: campaign.status,
+          ad_service_value: ad['status'],
+          consistent: statuses[campaign.status] == ad['status']
+        },
+        ad_description: {
+          local_value: campaign.ad_description,
+          ad_service_value: ad['description'],
+          consistent: campaign.ad_description == ad['description']
         }
-      ]
-    }.to_json
+      }
+      payload[:campaigns] << campaign_payload
+    end
 
     render json: payload
   end
